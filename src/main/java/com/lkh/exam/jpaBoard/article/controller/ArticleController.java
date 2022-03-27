4f7b777858f5d22225195bd786d1ee07e5f52895
Leekunhee830 <gorrka159@naver.com>
2022-03-27 17:30:44 +0900

    게시물 수정,삭제기능

--- src/main/java/com/lkh/exam/jpaBoard/article/controller/ArticleController.java
+++ src/main/java/com/lkh/exam/jpaBoard/article/controller/ArticleController.java
@@ -28,4 +28,30 @@ public class ArticleController {
        Optional<Article> article= articleRepository.findById(id);
         return article.get();
     }
+
+    @RequestMapping("doDelete")
+    @ResponseBody
+    public String doDelete(long id){
+        if(articleRepository.existsById(id)){
+            articleRepository.deleteById(id);
+            return "%d번째 게시물이 삭제되었습니다.".formatted(id);
+        }
+        return "%d번째 게시물이 존재하지 않습니다.".formatted(id);
+    }
+
+    @RequestMapping("doModify")
+    @ResponseBody
+    public Article showModify(long id,String title,String body){
+        Article article= articleRepository.findById(id).get();
+       if(title!=null){
+           article.setTitle(title);
+       }
+       if(body!=null){
+            article.setBody(body);
+       }
+
+       articleRepository.save(article);
+
+        return article;
+    }
 }
